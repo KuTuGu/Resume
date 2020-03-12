cbdfb8526f85311cc1a79d5c526b8e3a90b2abc3
xinhui.zxh <xinhui.zxh@antfin.com>
2020-03-12 12:06:57 +0800

    feat: update style

--- src/components/personalInfo.vue
+++ src/components/personalInfo.vue
@@ -3,7 +3,7 @@
         <div class="signature">{{Info.signature}}</div>
         <p class="name">{{Info.name}}</p>
         <p class="job">{{Info.job}}</p>
-        <div v-for="i of Info.intro" :key="i">{{i}}</div>
+        <div class="intro" v-for="i of Info.intro" :key="i">{{i}}</div>
     </div>
 </template>
 
@@ -26,6 +26,14 @@ export default {
     padding: 0 10% 20px 10px;
     border-bottom: solid 1px black;
 }
+.intro {
+    font-size: 14px;
+    line-height: 1;
+    padding-bottom: 10px;
+}
+.intro:last-child {
+    padding-bottom: 0px;
+}
 .signature{
     font-weight: bold;
     border: solid 1px black;
@@ -35,10 +43,10 @@ export default {
 .name{
     font-weight: bold;
     font-size: 30px;
-    margin: 15px 0;
+    margin: 10px 0;
 }
 .job{
-    font-size: 20px;
-    margin: 15px 0;
+    font-size: 18px;
+    margin: 10px 0;
 }
 </style>
